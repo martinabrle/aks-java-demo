2ee23d674d9f306383a741f81a06052f7ce77de0
martinabrle <martinabrle@users.noreply.github.com>
2023-12-05 07:35:22 +0100

    No unsigned support in pgsql

--- spring-petclinic-vets-service/src/main/resources/db/pgsql/schema.sql
+++ spring-petclinic-vets-service/src/main/resources/db/pgsql/schema.sql
@@ -1,19 +1,19 @@
 CREATE TABLE IF NOT EXISTS vets (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   INDEX(last_name)
 );
 
 CREATE TABLE IF NOT EXISTS specialties (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(80),
   INDEX(name)
 );
 
 CREATE TABLE IF NOT EXISTS vet_specialties (
-  vet_id INT UNSIGNED NOT NULL,
-  specialty_id INT UNSIGNED NOT NULL,
+  vet_id INT NOT NULL,
+  specialty_id INT NOT NULL,
   FOREIGN KEY (vet_id) REFERENCES vets(id),
   FOREIGN KEY (specialty_id) REFERENCES specialties(id),
   UNIQUE (vet_id,specialty_id)
