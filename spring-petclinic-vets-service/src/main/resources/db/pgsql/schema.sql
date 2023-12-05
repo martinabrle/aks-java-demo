63392d120d6917dac500907ceb52bcd9aa95f67d
martinabrle <martinabrle@users.noreply.github.com>
2023-12-05 07:44:36 +0100

    autoincrement -> serial

--- spring-petclinic-vets-service/src/main/resources/db/pgsql/schema.sql
+++ spring-petclinic-vets-service/src/main/resources/db/pgsql/schema.sql
@@ -1,19 +1,20 @@
 CREATE TABLE IF NOT EXISTS vets (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  INDEX(last_name)
+  "id" SERIAL PRIMARY KEY NOT NULL,
+  "first_name" VARCHAR(30),
+  "last_name" VARCHAR(30)
 );
 
+CREATE INDEX IF NOT EXISTS vets_last_name_idx ON vets ( "last_name" );
+
 CREATE TABLE IF NOT EXISTS specialties (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(80),
+  "id" SERIAL PRIMARY KEY NOT NULL ,
+  "name" VARCHAR(80),
   INDEX(name)
 );
 
 CREATE TABLE IF NOT EXISTS vet_specialties (
-  vet_id INT NOT NULL,
-  specialty_id INT NOT NULL,
+  "vet_id" INT NOT NULL,
+  "specialty_id" INT NOT NULL,
   FOREIGN KEY (vet_id) REFERENCES vets(id),
   FOREIGN KEY (specialty_id) REFERENCES specialties(id),
   UNIQUE (vet_id,specialty_id)
