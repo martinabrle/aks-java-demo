e13b22caf755abb414f30332966e9f95df61163e
martinabrle <martinabrle@users.noreply.github.com>
2023-12-05 07:15:37 +0100

    Add pgsql scripts

---
+++ spring-petclinic-vets-service/src/main/resources/db/pgsql/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS vets (
+  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  INDEX(last_name)
+);
+
+CREATE TABLE IF NOT EXISTS specialties (
+  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(80),
+  INDEX(name)
+);
+
+CREATE TABLE IF NOT EXISTS vet_specialties (
+  vet_id INT(4) UNSIGNED NOT NULL,
+  specialty_id INT(4) UNSIGNED NOT NULL,
+  FOREIGN KEY (vet_id) REFERENCES vets(id),
+  FOREIGN KEY (specialty_id) REFERENCES specialties(id),
+  UNIQUE (vet_id,specialty_id)
+);
